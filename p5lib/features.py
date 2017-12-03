9ae679c2dbf8cf35d88d35874511d9696a87a2f1
Efraim Kropp <efraim.kropp@flex.com>
2017-12-03 18:06:52 -0500

    new video

--- p5lib/features.py
+++ p5lib/features.py
@@ -37,10 +37,11 @@ def color_hist_origin(img, nbins=32, bins_range=(0, 256)):
 def color_hist(img, nbins=32):
     # Compute the histogram of the color channels separately
     channel1_hist, bin_edges = np.histogram(img[:,:,0], bins=nbins, density=True)
-    channel1_hist = channel1_hist * np.diff(bin_edges)
     channel2_hist, bin_edges = np.histogram(img[:,:,1], bins=nbins, density=True)
-    channel2_hist = channel2_hist * np.diff(bin_edges)
     channel3_hist, bin_edges = np.histogram(img[:,:,2], bins=nbins, density=True)
+    # Normalization - forum     
+    channel1_hist = channel1_hist * np.diff(bin_edges)
+    channel2_hist = channel2_hist * np.diff(bin_edges)
     channel3_hist = channel3_hist * np.diff(bin_edges)
     # Concatenate the histograms into a single feature vector
     hist_features = np.concatenate((channel1_hist, channel2_hist, channel3_hist))
@@ -64,7 +65,10 @@ def get_bin_spatial(img, color_space='RGB', size=(32, 32)):
     else: feature_image = np.copy(img)             
     # Use cv2.resize().ravel() to create the feature vector
     features = cv2.resize(feature_image, size).ravel() 
-    # Return the feature vector
+     # Normalize the range if needed - forum
+    if np.amax(features) > 1:
+        features = features / 255.
+   # Return the feature vector
     return features
 
 # Define a function to return HOG features and visualization
