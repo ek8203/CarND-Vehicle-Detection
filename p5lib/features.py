4ad7572545464321f843e1d01e496a227ce7fd58
Efraim Kropp <efraim.kropp@flex.com>
2017-12-03 10:52:49 -0500

    w/ norm features

--- p5lib/features.py
+++ p5lib/features.py
@@ -13,19 +13,37 @@ from skimage.feature import hog
 def bin_spatial(img, size=(32, 32)):
     
     # Use cv2.resize().ravel() to create the feature vector
-    features = cv2.resize(img, size).ravel() 
+    features = cv2.resize(img, size).ravel()
+    # Normalize the range if needed - forum
+    if np.amax(features) > 1:
+        features = features / 255.
     # Return the feature vector
     return features
 
 # Define a function to compute color histogram features 
 # NEED TO CHANGE bins_range if reading .png files with mpimg!
-def color_hist(img, nbins=32, bins_range=(0, 256)):
+def color_hist_origin(img, nbins=32, bins_range=(0, 256)):
     # Compute the histogram of the color channels separately
     channel1_hist = np.histogram(img[:,:,0], bins=nbins, range=bins_range)
     channel2_hist = np.histogram(img[:,:,1], bins=nbins, range=bins_range)
     channel3_hist = np.histogram(img[:,:,2], bins=nbins, range=bins_range)
     # Concatenate the histograms into a single feature vector
     hist_features = np.concatenate((channel1_hist[0], channel2_hist[0], channel3_hist[0]))
+    # Return the individual histograms, bin_centers and feature vector
+    return hist_features
+    
+# Define a function to compute color histogram features
+# The normalization is done according the forum     
+def color_hist(img, nbins=32):
+    # Compute the histogram of the color channels separately
+    channel1_hist, bin_edges = np.histogram(img[:,:,0], bins=nbins, density=True)
+    channel1_hist = channel1_hist * np.diff(bin_edges)
+    channel2_hist, bin_edges = np.histogram(img[:,:,1], bins=nbins, density=True)
+    channel2_hist = channel2_hist * np.diff(bin_edges)
+    channel3_hist, bin_edges = np.histogram(img[:,:,2], bins=nbins, density=True)
+    channel3_hist = channel3_hist * np.diff(bin_edges)
+    # Concatenate the histograms into a single feature vector
+    hist_features = np.concatenate((channel1_hist, channel2_hist, channel3_hist))
     # Return the individual histograms, bin_centers and feature vector
     return hist_features
 
@@ -56,15 +74,15 @@ def get_hog_features(img, orient, pix_per_cell, cell_per_block,
     if vis == True:
         features, hog_image = hog(img, orientations=orient, pixels_per_cell=(pix_per_cell, pix_per_cell),
                                   cells_per_block=(cell_per_block, cell_per_block), transform_sqrt=True, 
-                                  visualise=vis, feature_vector=feature_vec,
-                                  block_norm="L2-Hys")
+                                  visualise=vis, feature_vector=feature_vec)#,
+                                  #block_norm="L2-Hys")
         return features, hog_image
     # Otherwise call with one output
     else:      
         features = hog(img, orientations=orient, pixels_per_cell=(pix_per_cell, pix_per_cell),
                        cells_per_block=(cell_per_block, cell_per_block), transform_sqrt=True, 
-                       visualise=vis, feature_vector=feature_vec,
-                       block_norm="L2-Hys")
+                       visualise=vis, feature_vector=feature_vec)#,
+                       #block_norm="L2-Hys")
         return features
 
 # Define a function to extract features from a list of images
@@ -79,9 +97,9 @@ def extract_features(imgs, color_space='RGB', spatial_size=(32, 32),
     for file in imgs:
         file_features = []
         # Read in each one by one
-#        image = mpimg.imread(file)
-        image = cv2.imread(file)
-        image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+        image = mpimg.imread(file)
+        #image = cv2.imread(file)
+        #image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
         
         # apply color conversion if other than 'RGB'
         if color_space != 'RGB':
